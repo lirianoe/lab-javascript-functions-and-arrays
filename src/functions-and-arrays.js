2a6498f265bb6f84f3d14403fb2b74b40b4108b6
Mac <mac@Macs-MBP.attlocal.net>
2022-10-21 16:17:25 -0400

    lirianoe

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,20 +1,55 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 < num2){
+    return num2 
+    
+  }
+    else if (num1 > num2) {
+      return (num1)
+    }
+
+    else {
+      return (num1)
+    }
+  }
+   return maxOfTwoNumbers(8, 9)
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null
+  }
+
+  let longestWord = ""
+  for (let i = 0; i < words.length; i++) {
+    if (longestWord.length < words[i].length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord
+}
+
 
 
 
 // Iteration #3: Calculate the sum
+
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
-function sumNumbers() {}
-
+function sumNumbers(numbers) {
+  
+  
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    
+    sum += numbers[i];
+  }
+return sum
+}
+return sumNumbers(numbers)
 
 
 // Iteration #3.1 Bonus:
@@ -26,13 +61,37 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+
+
+function sumNumbers(numbers) {
+  
+  
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    
+    sum += numbers[i];
+  }
+return sum
+}
+return sumNumbers(numbers)
+
+
+
+
+function averageNumbers(numbersAvg) {
+  return sumNumbers(numbersAvg) / numbersAvg.length
+
+}
+return (averageNumbers(numbersAvg))
+
+
+
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+f
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -51,6 +110,7 @@ const wordsUnique = [
   'simple',
   'bring'
 ];
+
 
 function uniquifyArray() {}
 
